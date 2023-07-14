2fb71556ba3c7799f12a90acd3522b42d9411a85
Argha Das <arghadasofficial@gmail.com>
2023-07-14 10:42:11 +0530

    Added and Modified Login, AuthActivity, Dashboard and SignUp

--- app/src/main/java/argha/firebase/impl/AuthActivity.kt
+++ app/src/main/java/argha/firebase/impl/AuthActivity.kt
@@ -1,11 +1,54 @@
 package argha.firebase.impl
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import argha.firebase.impl.fragments.LoginListener
+import argha.firebase.impl.fragments.SignUpListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class AuthActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity(), LoginListener, SignUpListener {
+
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        init()
+    }
+
+    fun init() {
+        auth = Firebase.auth
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser != null) {
+            intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onLoginSuccess() {
+        intent = Intent(this, DashboardActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onLoginError(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onSignUpSuccess() {
+        // TODO
+    }
+
+    override fun onSignUpError(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
